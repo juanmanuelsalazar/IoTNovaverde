cfb84fb766ac653cc7bf49345383461d84259b7f
juanmanuelsalazar <juanm.salazar@udea.edu.co>
2024-03-13 13:55:24 -0500

    Config MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@
 #include "settingsRead.hpp"
 #include "settingsSave.hpp"
 #include "esp32_wifi.hpp"
+#include "esp32_mqtt.hpp"
 
 // -------------------------------------------------------------------
 // Setup
@@ -34,11 +35,16 @@ void setup() {
     log("\nInfo: Iniciando Setup");
     // Configurar los Pines
     settingPines();
-    // Inicio del SPIFFS                
-    if (!SPIFFS.begin(true)){
+    // Inicio del SPIFFS                 
+    if (!SPIFFS.begin()){
         log(F("Error: Falló la inicialización del SPIFFS"));
         while (true);
     }
+    // Lee los estados de los Relays
+    settingsReadRelays();
+    // Paso estados a los pines de los Relays
+    setOnOffSingle(RELAY1,Relay01_status);
+    setOnOffSingle(RELAY2,Relay02_status);
     // Lee la Configuración WiFi
     settingsReadWiFi();
     // Configuracion WIFI
@@ -46,6 +52,8 @@ void setup() {
     delay(1000);
     // Setup del WiFI
     wifi_setup(); 
+    // Lee la Configuración MQTT
+    settingsReadMQTT();
 }
 
 
@@ -63,6 +71,20 @@ void loop() {
     }else if (wifi_mode == WIFI_AP){
         wifiAPLoop();
     } 
+    // -------------------------------------------------------------------
+    // MQTT
+    // -------------------------------------------------------------------
+    if ((WiFi.status() == WL_CONNECTED) && (wifi_mode == WIFI_STA)){
+        if(mqtt_server != 0){
+            mqttLoop();
+            if (mqttclient.connected()){
+                if (millis() - lastMsg > mqtt_time){
+                    lastMsg = millis();
+                    mqtt_publish();
+                }
+            }      
+        }
+    }
 
 
 }
